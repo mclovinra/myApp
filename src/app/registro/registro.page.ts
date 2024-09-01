e0b7b174ef60c9d997e08f0173a66b9c517fd8e5
Rafu <ra.quichiz@duocuc.cl>
2024-09-01 00:27:28 -0400

    Registarse

--- src/app/registro/registro.page.ts
+++ src/app/registro/registro.page.ts
@@ -1,21 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-registro',
   templateUrl: './registro.page.html',
   styleUrls: ['./registro.page.scss'],
 })
+
 export class RegistroPage implements OnInit {
 
   formularioRegistro: FormGroup;
 
-  constructor(public fb: FormBuilder) {
+  constructor(public fb: FormBuilder, public alertController: AlertController) {
 
     this.formularioRegistro = this.fb.group({
-      'nombre': new FormControl("",Validators.required),
-      'contraseña': new FormControl("",Validators.required),
-      'confirmarContraseña': new FormControl("",Validators.required),
+      'nombre': new FormControl('',Validators.required),
+      'contraseña': new FormControl('',Validators.required),
+      'confirmarContraseña': new FormControl('',Validators.required)
     });
 
    }
@@ -23,4 +25,17 @@ export class RegistroPage implements OnInit {
   ngOnInit() {
   }
 
+  async guardar() {
+    var f = this.formularioRegistro.value;
+
+    if (this.formularioRegistro.invalid) {
+      const alert = await this.alertController.create({
+        message: 'Rellene Todos los Campos',
+        buttons: ['Aceptar']
+      });
+      await alert.present();
+      return;
+    }
+
+  }
 }
